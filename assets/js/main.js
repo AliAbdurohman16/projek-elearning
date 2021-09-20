48fbaa3ffb6c68057de10909b16de90aa2f881c6
Ali Abdurohman <aliabdurohman16@gmail.com>
2021-09-20 10:50:04 +0700

    updating

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,14 @@
+// Navbar
+var nav = document.querySelector('nav');
+
+window.addEventListener('scroll', function() {
+    if (window.pageYOffset > 100) {
+        nav.classList.add('bg-white', 'shadow');
+    } else {
+        nav.classList.remove('bg-white', 'shadow');
+    }
+})
+
 // Owl Carousel
 $(".owl-carousel").owlCarousel({
     autoplay: true,
